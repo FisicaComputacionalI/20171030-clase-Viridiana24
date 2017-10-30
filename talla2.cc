322c96c42d89fdae30c6561b01a0d1efbe36bbd9
Viridiana <31377504+Viridiana24@users.noreply.github.com>
2017-10-30 13:39:18 -0600

    Add files via upload

---
+++ talla2.cc
@@ -0,0 +1,24 @@
+//Saber la edad promedio a la que se graduan los estudiantes de la lic en fisica.
+//Barranco Diaz Carmen Viridiana 
+//30/10/17
+#include<vector>
+#include<iostream>
+using namespace std;
+int main(){
+
+ size_t talla=14;
+vector<int> edad(talla);
+	int suma=0;
+for(int i=0; i<talla; i++)
+{
+cout<<"¿A que edad te graduaste?"<<endl;
+cin>>edad[i];
+suma+=edad[i];
+}
+for(int j=0; j<talla; j++)
+	cout<<edad[j]<< ",";
+cout<<endl;
+cout<<"La edad promedio de graduacion esperado es"<<suma/talla<<endl;
+
+return 0;
+}
